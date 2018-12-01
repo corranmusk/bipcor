d858fa73b88021a98aa648b330e2c59ff50c7132
Corran Musk <corran@yoda.localdomain>
2018-12-01 21:42:38 +0000

    Started defining database

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
 from .nav import nav
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 nav.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
@@ -14,4 +18,5 @@ app.register_blueprint(errors_bp)
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
 
-#from app import routes
+#from app.auth import bp as auth_bp
+#app.register_blueprint(auth_bp)
